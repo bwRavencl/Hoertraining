4b9c2eb862ff24aa634692c353176019e348cca5
Matteo Hausner <matteo.hausner@gmail.com>
2026-06-30 21:43:03 +0200

    style: reformat files

--- sw.js
+++ sw.js
@@ -1,22 +1,15 @@
 const CACHE_NAME = "ear-training-cache-v1";
 
-const ASSETS = [
-    "./",
-    "./index.html",
-    "./manifest.json",
-    "./icon.png"
-];
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.png"];
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-    );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
 });
